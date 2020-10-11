f40237d9255371cb80909ac764899f6f97dd4697
silverskg <chokichoki_gm@i.softbank.jp>
2020-10-11 17:55:27 +0900

    期間指定ボタン作成

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,14 +1,23 @@
 document.addEventListener('turbolinks:load', () => {
+
+    const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
+    const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
+
     const convertDate = (date) => new Date(new Date(date).setHours(0,0,0,0))
+
+    const START_DATE = convertDate(gon.weight_records[0].date)
+    const END_DATE = convertDate(gon.weight_records[gon.weight_records.length - 1].date)
 
     const TODAY = convertDate(new Date())
     const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
-    const TWO_WEEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 13) 
+    const TWO_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 13) 
     const A_MONTH_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 1, TODAY.getDate() + 1)
-    const TWO_MONTH_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 3, TODAY.getDate() + 1)
+    const THREE_MONTH_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 3, TODAY.getDate() + 1)
 
      // グラフを描く場所を取得
     const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
+
+    let chartWeight
 
     // 期間を指定してグラフを描く
     const drawGraph = (from, to) => {
@@ -47,18 +56,46 @@ document.addEventListener('turbolinks:load', () => {
               return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, ' $1 月 $2 日')
             },
             label: function (tooltipItems){
-              return '体重: ' + tooltipItem.yLabel + 'kg'
+              return '体重: ' + tooltipItems.yLabel + 'kg'
             }
           }
         }
       }
 
-      new Chart(chartWeightContext, {
-        type: 'line',
-        data: weightData,
-        options: weightOption
-      })
+      if (!chartWeight) {
+        chartWeight = new Chart(chartWeightContext, {
+          type: 'line',
+          data: weightData,
+          options: weightOption
+        })
+      }else{
+        chartWeight.data = weightData
+        chartWeight.options = weightOption
+        chartWeight.update()
+      }
     }
+
+    const drawGraphToToday = (from) => {
+      form = maxDate(from, START_DATE)
+      let to = minDate(TODAY,END_DATE)
+      drawGraph(from, to)
+    }
+
+    document.getElementById('a-week-button').addEventListener('click', () =>{
+      drawGraphToToday(A_WEEK_AGO)
+    })
+
+    document.getElementById('two-week-button').addEventListener('click', () =>{
+      drawGraphToToday(TWO_WEEK_AGO)
+    })
+
+    document.getElementById('a-month-button').addEventListener('click', () =>{
+      drawGraphToToday(A_MONTH_AGO)
+    })
+
+    document.getElementById('three-months-button').addEventListener('click', () =>{
+      drawGraphToToday(THREE_MONTH_AGO)
+    })
     
     // グラフの初期表示
     drawGraph(A_WEEK_AGO, TODAY)
